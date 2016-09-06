0dfed8cffcee1810e33b396c0323382e0fa58e36
Rene Reichenbach <rene.reichenbach@zalando.de>
2016-09-06 15:29:35 +0200

    further fixes and README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# golang-application-properties
+# golangapplicationproperties
 Provides easy access to Java style property files
+
+GET
+
+```bash
+go get "github.com/DaKine23/golangapplicationproperties"
+```
+IMPORT
+
+```golang
+import "github.com/DaKine23/golangapplicationproperties"
+```
+
+USAGE (simple)
+
+- place your file wherever you like most likely next to your go program
+- e.g. **application.properties**
+- add some values like **my.value = 1234** to the files
+
+```golang
+var p *golangapplicationproperties.Properties
+// Init with filename and debugging false
+ap = ap.Init("./application.properties", false)
+//get properties as a map m [string] sting
+m := ap.GetProps()
+// should print "1234"
+println(m["my.value"])
+```
+
+USAGE
+
+```golang
+var p *golangapplicationproperties.Properties
+// get currently loaded properties (may somewhere else in the code)
+ap := p.Current()
+// check for "IsInitialized"
+if !ap.IsInitialized() {
+    //only init if not already done
+    ap = ap.Init("./application.properties", true)
+}
+//get properties as a map m [string] sting
+m := ap.GetProps()
+// should print "1234"
+println(m["my.value"])
+```
--- golangapplicationproperties.go
+++ golangapplicationproperties.go
@@ -5,24 +5,30 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
+// ApplicationProperties is the Singleton Value for props to init them only once systemwide
 var ApplicationProperties Properties
 
+// Properties is the type of ApplicationProperties the map contains all the data from the file
 type Properties struct {
 	FilePath    string
 	PropertyMap map[string]string
+	InitTime    time.Time
+	Debug       bool
 }
 
-func check(e error) {
-	if e != nil {
-		fmt.Print("check :")
+func (ap *Properties) check(e error) {
+	if e != nil && ap.Debug {
+		fmt.Print("GAP debug :")
 		fmt.Println(e)
 	}
 }
 
 var propertyMap = make(map[string]string)
 
+// GetProps returns the content of the property file that was read at InitTime
 func (ap *Properties) GetProps() map[string]string {
 
 	return ap.PropertyMap
@@ -32,12 +38,21 @@ func (ap *Properties) setFilePath(newFilePath string) {
 	ap.FilePath = newFilePath
 }
 
-func (ap *Properties) Init(newFilePath string) Properties {
+// Init initialize the properties usually only needs to be done once the property file changes or application starts
+func (ap *Properties) Init(newFilePath string, debug bool) Properties {
+	ApplicationProperties.Debug = debug
 	ApplicationProperties.setFilePath(newFilePath)
 	ApplicationProperties.readProps()
+	ApplicationProperties.InitTime = time.Now().Local().UTC()
 	return ApplicationProperties
 }
 
+//GetInitTime gets the time when last init has taken place
+func (ap *Properties) GetInitTime() time.Time {
+	return ap.InitTime
+}
+
+// Current gets the ApplicationProperties Singleton Object
 func (ap *Properties) Current() Properties {
 	return ApplicationProperties
 }
@@ -47,18 +62,22 @@ func (ap *Properties) readProps() error {
 	ap.PropertyMap = make(map[string]string)
 
 	file, err := os.Open(ap.FilePath)
-	check(err)
+	ap.check(err)
 
 	reader := bufio.NewReader(file)
 
 	for {
 
 		lineAsBytes, _, err := reader.ReadLine()
+
 		if err != nil {
+			if err.Error() != "EOF" {
+				ap.check(err)
+			}
 			break
 		}
 		lineAsString := strings.Split(string(lineAsBytes), "=")
-		check(err)
+		ap.check(err)
 		if len(strings.TrimSpace(lineAsString[0])) > 0 {
 			ap.PropertyMap[strings.TrimSpace(lineAsString[0])] = strings.TrimSpace(lineAsString[len(lineAsString)-1])
 		}
@@ -70,18 +89,7 @@ func (ap *Properties) readProps() error {
 
 }
 
+//IsInitialized tells you if the property file was already loaded
 func (ap *Properties) IsInitialized() bool {
 	return (len(ap.PropertyMap) > 0)
 }
-
-//func main() {
-//	a, b := IsInitialized()
-//	println(a, b)
-//	Init("./application.properties")
-//	a, b = IsInitialized()
-//	println(a, b)
-//	m := GetProps()
-
-//	println(m["zalos.sproc.datasource.maxConnectionAgeInSeconds"])
-
-//}
